3f6084ffa8773477aedb71bf1ad614ed3c752517
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-11-18 18:17:36 +0200

    add transactions and finish routes

--- http/espresso.js
+++ http/espresso.js
@@ -5,6 +5,7 @@ class Espresso {
   constructor() {
     this.server = http.createServer();
     this.routes = {};
+    this.middleware = [];
 
     this.server.on("request", (req, res) => {
       // Send a file back to the client
@@ -30,19 +31,35 @@ class Espresso {
         res.end(JSON.stringify(data));
       };
 
-      // If the routes object does not have a key of req.method + req.url, return 404
-      if (!this.routes[req.method.toLocaleLowerCase() + req.url]) {
-        return res
-          .status(404)
-          .json({ error: `Cannot ${req.method} ${req.url}` });
-      }
+      // Run all the middleware functions before we run the corresponding route
+      const runMiddleware = (req, res, middleware, index) => {
+        // Out exit point...
+        if (index === middleware.length) {
+          // If the routes object does not have a key of req.method + req.url, return 404
+          if (!this.routes[req.method.toLocaleLowerCase() + req.url]) {
+            return res
+              .status(404)
+              .json({ error: `Cannot ${req.method} ${req.url}` });
+          }
 
-      this.routes[req.method.toLocaleLowerCase() + req.url](req, res);
+          this.routes[req.method.toLowerCase() + req.url](req, res);
+        } else {
+          middleware[index](req, res, () => {
+            runMiddleware(req, res, middleware, index + 1);
+          });
+        }
+      };
+
+      runMiddleware(req, res, this.middleware, 0);
     });
   }
 
   route(method, path, cb) {
     this.routes[method + path] = cb;
+  }
+
+  beforeEach(cb) {
+    this.middleware.push(cb);
   }
 
   listen(port, cb) {
